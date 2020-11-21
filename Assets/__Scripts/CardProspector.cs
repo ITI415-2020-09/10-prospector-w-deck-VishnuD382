0009b81ac5c786c4c32d0d96d949d8e9e63acdea
VishnuD382 <vishnudhanasekaran9@gmail.com>
2020-11-21 18:36:34 -0500

    Getting Cards to show up in DrawPile

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eCardState
+{
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+public class CardProspector : Card
+{
+    [Header("Set Dynamically: CardProspector")]
+    public eCardState state = eCardState.drawpile;
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    public int layoutID;
+    public SlotDef slotDef;
+
+    //override public void onmouseupasbutton()
+    //{
+    //    prospector.s.cardclicked(this);
+    //    base.onmouseupasbutton();
+    //}
+
+}
